35bc1dde488f4d6e9f1e082546ccdd3838e893e6
Aryan Shah <aryanshah791@gmail.com>
2021-03-18 22:56:45 -0400

    Refactor key

--- places/lib/places_web/controllers/page_controller.ex
+++ places/lib/places_web/controllers/page_controller.ex
@@ -2,13 +2,16 @@ defmodule PlacesWeb.PageController do
   use PlacesWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", data: nil)
   end
 
   def search(conn, %{"search" => search_query}) do
+    key = Application.fetch_env!(:external_service, :api_key)
     lat = "42.3601"
     lng = "-71.0589"
-    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}&rankby=distance&name=#{search_query}&types=cafe&key=AIzaSyBPJQF7VI-_AM85UZyiBrzI8GL3U5wQcF8"
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+    <> "location=#{lat},#{lng}&rankby=distance&name=#{search_query}&"
+    <> "types=cafe&key=#{key}"
   
     resp = HTTPoison.get!(url)
     data = Jason.decode!(resp.body)
